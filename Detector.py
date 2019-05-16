cdaf136e5a35d4e8122dd01c1084b5b84fa0f40f
gfreychet <gfreychet@gmail.com>
2019-05-16 16:06:24 -0400

    Creation of a detector file

---
+++ Detector.py
@@ -0,0 +1,62 @@
+import numpy as np
+from pyFAI import detectors
+from pyFAI.detectors import Pilatus300kw, Pilatus1M
+
+
+class Pilatus1M_SMI(Pilatus1M):
+
+    def calc_mask(img, bs=None):
+        mask = np.logical_not(detectors.Pilatus1M().calc_mask())
+        mask[:, :5], mask[:, -5:], mask[:5, :], mask[-5:, :] = False, False, False, False
+
+        # bad pixels
+        mask[20, 884], mask[56, 754], mask[111, 620], mask[145, 733], mask[178, 528], mask[
+            189, 571] = False, False, False, False, False, False
+        mask[372, 462], mask[454, 739], mask[657, 947], mask[869, 544], mask[870, 546], mask[
+            870, 547] = False, False, False, False, False, False
+        mask[870, 544], mask[871, 545], mask[871, 546], mask[871, 547] = False, False, False, False
+
+        # beamstop
+        mask[bs[0] + 40:, bs[1] - 11:bs[1] + 11] = False
+        mask[bs[0]:bs[0] + 40, bs[1] - 22:bs[1] + 22] = False
+        return mask
+
+
+class VerticalPilatus300kw(Pilatus300kw):
+    MAX_SHAPE = (1475, 195)
+    MODULE_SIZE = (487, 195)
+    MODULE_GAP = (7, 17)
+    aliases = ["Pilatus 300kw (Vertical)"]
+
+    def calc_mask(self, bs):
+        mask = np.rot90(np.logical_not(detectors.Pilatus300kw().calc_mask()), 1)
+
+        # Dead pixel
+        dead_pix_x = [228, 307, 733, 733, 792, 1211, 1211, 1231, 1232, 1276, 1321, 1366, 1405, 1467]
+        dead_pix_y = [21, 67, 170, 171, 37, 109, 110, 74, 74, 57, 81, 181, 46, 188]
+        for d_x, d_y in zip(dead_pix_x, dead_pix_y):
+            mask[d_x, d_y] = False
+
+        # Hot pixels
+        mask[1314, 81] = False
+        mask[732, 7], mask[732, 8], mask[733, 8], mask[733, 7], mask[733, 9] = False, False, False, False, False
+        mask[1314, 82], mask[1315, 81] = False, False
+
+        mask[674, 133], mask[674, 134], mask[1130, 20], mask[1239, 50] = False, False, False, False
+
+        # Beamstop
+        mask[bs[0]:, bs[1] - 11:bs[1] + 11] = False
+
+        return mask
+
+
+# TODO: define rayonix class
+class Rayonix(Pilatus300kw):
+    MAX_SHAPE = (1475, 195)
+    MODULE_SIZE = (487, 195)
+    MODULE_GAP = (7, 17)
+    aliases = ["Pilatus 300kw (Vertical)"]
+
+    def calc_mask(self, pixel_bs):
+        mask = 0
+        return mask
--- SMI_test.py
+++ SMI_test.py
@@ -1,15 +1,15 @@
-import numpy as np
+#import numpy as np
 import fabio
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 import stitch, remesh, SMI_beamline
 
 if __name__ == '__main__':
 
-
     #coming from the document:
-    geometry, sdd, wav, alphai, center, bs_pos, detector = 'Transmission', 273.9, 1E-9 * (12.39842/2.4), np.deg2rad(0.0), [96., 1475-89.5], [7, 1330], 'Pilatus300kw'
+    geometry, sdd, wav, alphai, center, bs_pos, detector = 'Transmission', 273.9, 1E-9 * (12.39842/2.4), np.deg2rad(0.0), [96., 1475-89.5], [1310, 15], 'Pilatus300kw'
 
     det_ini_angle, det_angle_step = np.deg2rad(3.25), np.deg2rad(6.)
 
@@ -17,33 +17,29 @@ if __name__ == '__main__':
     SMI.calculate_integrator_trans()
 
 
-    dat = []
-    path = '/home/guillaume/Desktop/xiaodan_data'
+    path = 'C:\\Users\\gfreychet\\Desktop\\smi_data\\test_multigeo'
 
     energies = ['2465']
     dat = [[]] * len(energies)
 
     for i, ener in enumerate(energies):
         for file in os.listdir(path):
-            if 'GF_DPPT_%seV_waxsscan_npos_'%ener in file:
+            if '.tif' in file:
                 print(file)
                 dat[i] = dat[i] + [os.path.join(path, file)]
 
 
-    i = 0
     plt.figure()
-    for da in np.sort(dat[0]):
+    for i, da in enumerate(np.sort(dat[0])):
         plt.subplot(1, 6, i+1)
         plt.imshow(np.log(np.rot90(fabio.open(os.path.join(path, da)).data, 1)))
-        i+=1
     plt.show()
 
 
-    print(dat[0][0])
-    SMI.stitching_data(path, [dat[0][0]])
+    SMI.stitching_data(path, dat[0])
 
     plt.figure()
-    plt.imshow(np.log(SMI.img_st), extent = [SMI.qp[0], SMI.qp[1], SMI.qz[0], SMI.qz[1]], vmin = 0.01, vmax = 10)
+    plt.imshow(np.log(SMI.img_st), extent=[SMI.qp[0], SMI.qp[1], SMI.qz[0], SMI.qz[1]], vmin=0.01, vmax=10)
     plt.show()
 
 
