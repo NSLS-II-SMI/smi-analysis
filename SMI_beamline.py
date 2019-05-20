4a925ee4772847b28419202bd880e84f8cddd31c
gfreychet <gfreychet@gmail.com>
2019-05-20 11:45:00 -0400

    Rework the shipping of the Integrator: Mask and detector rotation included in the Integrator

--- SMI_beamline.py
+++ SMI_beamline.py
@@ -27,7 +27,7 @@ class SMI_geometry():
         self.det_ini_angle = det_ini_angle
         self.det_angle_step = det_angle_step
 
-        self.ai = None
+        self.ai = []
 
         self.define_detector()
 
@@ -36,10 +36,6 @@ class SMI_geometry():
     def define_detector(self):
         if self.detector == 'Pilatus1m':
             self.det = Detector.Pilatus1M_SMI()
-
-            self.det_ini_angle = 0
-            self.det_angle_step = 0
-
             self.mask = self.det.calc_mask(bs=self.bs)
 
         elif self.detector == 'Pilatus300kw':
@@ -53,52 +49,55 @@ class SMI_geometry():
         else:
             raise Exception('Unknown detector for SMI')
 
-    #TODO: ship the mask with the azimuthal integrator
-    #TODO: update the rot1 for each image and ship a list of images and ais
-    def calculate_integrator_trans(self):
-        self.ai = azimuthalIntegrator.AzimuthalIntegrator(**{'detector': self.det,
+
+    def calculate_integrator_trans(self, det_rot):
+        ai = azimuthalIntegrator.AzimuthalIntegrator(**{'detector': self.det,
                                     'rot1':0,
                                     'rot2':0,
                                     'rot3':0})
 
-        self.ai.setFit2D(self.sdd, self.center[0], self.center[1])
-        self.ai.set_wavelength(self.wav)
+        ai.setFit2D(self.sdd, self.center[0], self.center[1])
+        ai.set_rot1(det_rot)
+        ai.set_wavelength(self.wav)
+        ai.set_mask(self.mask)
+        return ai
 
 
-    def calculate_integrator_gi(self):
-        self.ai = Transform(wavelength=self.wav, detector=self.det)
-        self.ai.setFit2D(directDist= self.sdd, centerX=self.center[0], centerY=self.center[1])
-        self.ai.set_incident_angle(self.alphai)
+    def calculate_integrator_gi(self, det_rot):
+        ai = Transform(wavelength=self.wav, detector=self.det)
+        ai.set_rot1(det_rot)
+        ai.setFit2D(directDist= self.sdd, centerX=self.center[0], centerY=self.center[1])
+        ai.set_incident_angle(self.alphai)
+        ai.set_mask(self.mask)
+        return ai
 
-
-    def stitching_data(self, path, file):
+    def stitching_data(self, path, files):
         if self.geometry == 'Transmission':
-            if self.ai == None:
-                self.calculate_integrator_trans()
+            if self.ai == []:
+                for i, file in enumerate(files):
+                    det_rot = self.det_ini_angle + i * self.det_angle_step\
+                    self.ai.append(self.calculate_integrator_trans(det_rot))
+
             self.img_st, self.qp, self.qz = stitch.stitching_waxs(path,
-                                                                  file,
-                                                                  self.det_ini_angle,
-                                                                  self.det_angle_step,
-                                                                  self.ai,
-                                                                  self.mask)
+                                                                  files,
+                                                                  self.ai)
+
 
         elif self.geometry== 'Reflection':
             if self.ai == None:
-                self.calculate_integrator_gi()
+                for i, file in enumerate(files):
+                    det_rot = self.det_ini_angle + i * self.det_angle_step
+                    self.ai.append(self.calculate_integrator_gi(det_rot))
+
             self.img_st, self.qp, self.qz = stitch.stitching_giwaxs(path,
-                                                                    file,
-                                                                    self.det_ini_angle,
-                                                                    self.det_angle_step,
-                                                                    self.ai,
-                                                                    self.mask)
+                                                                    files,
+                                                                    self.ai)
+
         else:
             raise Exception('Unknown geometry')
 
 
-    #TODO: Should all the images treated as a multigeometry: test if any difference
-    #TODO: Comparison of the radial averaging image by image and multigeometry
     #TODO: Start playing with pygix for 1D cuts and radial, azimuthal averaging
-    #TODO: Start looking into pygix multigeometry
     def radial_averaging(self, path, file):
         if self.geometry == 'Transmission':
             if self.ai == None:
