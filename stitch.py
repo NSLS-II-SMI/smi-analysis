dcfd87791eb55b1cff244c2e6c8b6125bae3521e
gfreychet <gfreychet@gmail.com>
2019-05-22 19:46:28 -0400

    Modification of the shipped arguments of the stitch function as well as the q values return for GI configuration

--- stitch.py
+++ stitch.py
@@ -2,21 +2,20 @@ import numpy as np
 import fabio
 import os
 import remesh
+import matplotlib.pyplot as plt
 
 
-def stitching_giwaxs(path, file, ais):
+def stitching_giwaxs(datas, ais):
 
-    for i, fi, ai in enumerate(zip(file, ais)):
-        data = np.rot90(fabio.open(os.path.join(path, fi)).data, 1)
+    for i, (data, ai) in enumerate(zip(datas, ais)):
         img, x, y = remesh.remesh_gi(data, ai, method='splitbbox', mask=ai.mask)
 
         if i == 0:
-            qimg = np.zeros((np.shape(img)[0], np.shape(img)[1], len(file)))
-            q_p_ini, q_z_ini = np.zeros((np.shape(x)[0], len(file))), np.zeros((np.shape(y)[0], len(file)))
+            qimg = np.zeros((np.shape(img)[0], np.shape(img)[1], len(datas)))
+            q_p_ini, q_z_ini = np.zeros((np.shape(x)[0], len(datas))), np.zeros((np.shape(y)[0], len(datas)))
             qimg[:, :np.shape(img)[1], i] = img
 
-        img1 = np.rot90(img, 2)
-        qimg[:, :np.shape(img)[1], i] = np.rot90(img, 2)
+        qimg[:, :np.shape(img)[1], i] = img
         q_p_ini[:len(x), i] = -x[::-1]
         q_z_ini[:len(y), i] = y[::-1]
 
@@ -29,9 +28,7 @@ def stitching_giwaxs(path, file, ais):
     qz_remesh = np.linspace(min(q_z_ini[:, 0]), max(q_z_ini[:, -1]), int(
         (nb_point + 1) * (max(q_z_ini[:, -1]) - min(q_z_ini[:, 0])) / (max(q_p_ini[:, -1]) - min(q_p_ini[:, 0]))))
 
-    for i, fi, ai in enumerate(zip(file, ais)):
-        data = np.rot90(fabio.open(os.path.join(path, fi)).data, 1)
-
+    for i, (data, ai) in enumerate(zip(datas, ais)):
         qp_start = np.argmin(abs(qp_remesh - np.min(q_p_ini[:, i])))
         qp_stop = np.argmin(abs(qp_remesh - np.max(q_p_ini[:, i])))
 
@@ -40,12 +37,12 @@ def stitching_giwaxs(path, file, ais):
         op_range = (qz_remesh[0], qz_remesh[-1])
 
         img, x, y = remesh.remesh_gi(data, ai, npt=npt, ip_range=ip_range, op_range=op_range, method='splitbbox', mask=ai.mask)
-        qimage = np.rot90(img, 2)
+        qimage = img
         qp, qz = -x[::-1], y[::-1]
 
         if i == 0:
-            qimg = np.zeros((np.shape(qimage)[0], np.shape(qimage)[1], len(file)))
-            q_p, q_z = np.zeros((np.shape(qp)[0], len(file))), np.zeros((np.shape(qz)[0], len(file)))
+            qimg = np.zeros((np.shape(qimage)[0], np.shape(qimage)[1], len(datas)))
+            q_p, q_z = np.zeros((np.shape(qp)[0], len(datas))), np.zeros((np.shape(qz)[0], len(datas)))
             qimg[:, :np.shape(qimage)[1], i] = qimage
             q_p[:len(qp), i] = qp
             q_z[:len(qz), i] = qz
@@ -67,10 +64,10 @@ def stitching_giwaxs(path, file, ais):
             sca1[:, qp_start: qp_start + np.shape(qimage)[1]] += (qimage >= 1).astype(int)
 
             img1 = np.ma.masked_array(img_te, mask=sca1 != 2 * sca)
-            img1 = ma.masked_where(img1 < 1, img1)
+            img1 = np.ma.masked_where(img1 < 1, img1)
             img_te[:, qp_start:qp_start + np.shape(qimage)[1]] += qimage
             img2 = np.ma.masked_array(img_te, mask=sca1 != 2 * sca)
-            img2 = ma.masked_where(img2 < 1, img2)
+            img2 = np.ma.masked_where(img2 < 1, img2)
 
             scale *= abs(np.mean(img2) - np.mean(img1)) / np.mean(img1)
             sca[:, qp_start:  qp_start + np.shape(qimage)[1]] += (qimage >= 1).astype(int)
@@ -78,18 +75,17 @@ def stitching_giwaxs(path, file, ais):
 
 
     img_fin = img_te / sca2
-    qp = [0.1 * qp_remesh.min(), 0.1 * qp_remesh.max()]
-    qz = [0.1 * qz_remesh.min(), 0.1 * qz_remesh.max()]
+    qp = [-qp_remesh.max(), -qp_remesh.min()]
+    qz = [-qz_remesh.max(), -qz_remesh.min()]
     return img_fin, qp, qz
 
 
-def stitching_waxs(path, file, ais):
+def stitching_waxs(datas, ais):
 
-    for i, (fi, ai) in enumerate(zip(file, ais)):
-        data = np.rot90(fabio.open(os.path.join(path, fi)).data, 1)
+    for i, (data, ai) in enumerate(zip(datas, ais)):
         img, x, y = remesh.remesh_transmission(data, ai, alphai=0, mask = ai.mask)
         if i == 0:
-            q_p_ini, q_z_ini = np.zeros((np.shape(x)[0], len(file))), np.zeros((np.shape(y)[0], len(file)))
+            q_p_ini, q_z_ini = np.zeros((np.shape(x)[0], len(datas))), np.zeros((np.shape(y)[0], len(datas)))
         q_p_ini[:len(x), i] = x
         q_z_ini[:len(y), i] = y
 
@@ -102,8 +98,7 @@ def stitching_waxs(path, file, ais):
     qz_remesh = np.linspace(min(q_z_ini[:, 0]), max(q_z_ini[:, -1]), int(
         (nb_point + 1) * (max(q_z_ini[:, -1]) - min(q_z_ini[:, 0])) / (max(q_p_ini[:, -1]) - min(q_p_ini[:, 0]))))
 
-    for i, (fi, ai) in enumerate(zip(file, ais)):
-        data = np.rot90(fabio.open(os.path.join(path, fi)).data, 1)
+    for i, (data, ai) in enumerate(zip(datas, ais)):
 
         qp_start = np.argmin(abs(qp_remesh - np.min(q_p_ini[:, i])))
         qp_stop = np.argmin(abs(qp_remesh - np.max(q_p_ini[:, i])))
