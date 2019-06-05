ecb757e94d823b360631901fd251cb02667de4d5
gfreychet <gfreychet@gmail.com>
2019-06-05 14:43:22 -0400

    Add the package installation in the setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,20 @@ from setuptools import setup
 setup(
     name='SMI_analysis',
     version='1.0',
-    packages=[''],
+    packages=['scipy', 'numpy', 'fabio', 'matplotlib', 'pyFAI', 'git+https://github.com/ronpandolfi/pygix.git'],
     url='',
     license='',
     author='gfreychet',
     author_email='gfreychet@gmail.com',
     description=''
 )
+
+
+
+install_requires=['scipy', 'Cython', 'pyFAI==0.16.0', 'h5py', 'PySide==1.2.2', 'pyqtgraph', 'QDarkStyle',
+
+                      'Pillow', 'pyfits', 'PyOpenGL', 'PyYAML', 'qtconsole','tifffile','pysftp',
+
+                      'requests','dask','distributed','appdirs','futures','scikit-image','imageio','vispy',
+
+                      'pypaws>=0.8.4','matplotlib', 'astropy'],
