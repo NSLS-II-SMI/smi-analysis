7b8daf2f6578ba6a1083db8fbc87b8847fa36851
gfreychet <gfreychet@gmail.com>
2019-06-05 14:18:46 -0400

    Creation of a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SMI_analysis',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='gfreychet',
+    author_email='gfreychet@gmail.com',
+    description=''
+)
