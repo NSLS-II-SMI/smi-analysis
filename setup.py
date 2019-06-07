99049e2e80fa0e1b57cf12b2a8707346ba3ef98e
gfreychet <gfreychet@gmail.com>
2019-06-06 23:10:09 -0400

    test install package from setup with git+

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='SMI_analysis',
     version='1.0',
     py_modules=['SMI_beamline', 'remesh', 'stitch', 'integrate1D', 'Detector'],
-    install_requires=['numpy', 'scipy', 'pyFAI'],
+    install_requires=['numpy', 'scipy', 'pyFAI', 'git+https://github.com/ronpandolfi/pygix.git'],
     url='',
     license='',
     author='gfreychet',
