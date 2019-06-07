21c552b2646cdbb9daee0002d562a5319dab32a6
gfreychet <gfreychet@gmail.com>
2019-06-06 23:05:22 -0400

    test install package from setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='SMI_analysis',
     version='1.0',
     py_modules=['SMI_beamline', 'remesh', 'stitch', 'integrate1D', 'Detector'],
+    install_requires=['numpy', 'scipy', 'pyFAI'],
     url='',
     license='',
     author='gfreychet',
