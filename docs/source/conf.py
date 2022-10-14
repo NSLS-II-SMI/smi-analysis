bc9dc0e22b6b2203fa4c2d2d0b5ebb7394d8be22
Rodrigo Telles <rtellez700@gmail.com>
2022-10-14 16:24:45 -0700

    update path again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,10 @@
 #
 import os
 import sys
-print(">>> os.path.abspath('../')", os.path.abspath('../'))
-print(">>> os.path.abspath('../../')", os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../'))
+
+print(">>> os.path.abspath('../../')", os.path.abspath('../../'), [f.split('/')[-1] for f in os.listdir(os.path.abspath('../../'))])
+sys.path.insert(0, os.path.abspath('../../'))
+
 
 
 # -- General configuration ------------------------------------------------
